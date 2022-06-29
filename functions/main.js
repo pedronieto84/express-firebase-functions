75668e2eaaa48d9f83c92f9a83c454e9a7bd7070
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-06-29 15:19:39 +0200

    Pongo console logs

--- functions/main.js
+++ functions/main.js
@@ -3,11 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    console.log('hola')
   res.send('Hello World!')
 })
 
 app.get('/bye', (req, res) => {
-    res.send('Good bye!')
+    console.log('adios')
+    res.send('Adios')
   })
 
 
