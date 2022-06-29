ad7487ef7e432ff7bd6e9a77359d7fc935000473
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-06-29 15:11:35 +0200

    Predeploy sintax para modificar el archivo de forma que express pueda funcionar en firebase-funcitons.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 
+const app = require("./main")
+
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
@@ -7,3 +9,6 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+
+exports.api = functions.https.onRequest(app)
--- functions/main.js
+++ functions/main.js
@@ -10,8 +10,5 @@ app.get('/bye', (req, res) => {
     res.send('Good bye!')
   })
 
-app.listen(port, () => { // REMOVE-ON-DEPLOY
-  console.log(`Example app listening on port ${port}`) // REMOVE-ON-DEPLOY
-}) // REMOVE-ON-DEPLOY
 
 module.exports = app
