f3a24f2ce963df55d7d332d994f7d06a5c5dcbb2
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-11-25 09:09:28 +0100

    Primera versión Beta de una Api rest simple

--- functions/main.js
+++ functions/main.js
@@ -4,18 +4,45 @@ const port = 3000
 
 app.get('/', (req, res) => {
     console.log('hola')
-  res.send('Hello World!')
+  res.send('Api de usuarios!')
+})
+// Obtener todos los usuarios
+app.get('/users', (req, res) => {
+  console.log('adios')
+  res.send('Adios')
 })
 
-app.get('/get', (req, res) => {
-    console.log('adios')
-    res.send({metodo: 'get'})
-  })
+// Crear un usuario
+app.post('/user', (req, res) => {
 
-  app.post('/post', (req, res) => {
-    console.log('adios')
-    const data = req.body
-    res.send({data, method:'post'})
-  })
+
+  const user = req.body
+  res.send({user})
+})
+
+// Obtener un usuario en concreto
+app.get('/user/:id', async (req, res) => {
+  const id = req.params.id
+  const user = await db.collection('users').doc(id).get()
+  res.send(`Get ${id} `)
+})
+
+// Updatear un usuario en concreto
+app.put('/user/:id', async (req, res) => {
+  const id = req.params.id
+  const user = req.body
+  const updateOperation = await db.collection('users').doc(id).update( user , { merge: true})
+  res.send({updateOperation})
+})
+
+// Eliminar un usuario en concreto
+app.delete('/user/:id', (req, res) => {
+  const id = req.params.id
+  
+  res.send(`Delete${id} `)
+})
+
+  
+
 
 module.exports = app
--- index.js
+++ index.js
@@ -2,15 +2,58 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const admin = require("firebase-admin");
+admin.initializeApp()
+const db = admin.firestore()
+
 app.get('/', (req, res) => {
     console.log('hola')
-  res.send('Hello World!')
+  res.send('Api de usuarios!')
+})
+// Obtener todos los usuarios
+app.get('/users', async (req, res) => {
+  const users = await db.collection('users').get()
+  const objectsOfUsers = users.docs.map((d)=> {
+    return {
+      user: d.data(),
+      id: d.id
+    }
+    
+  })
+  res.send(objectsOfUsers)
 })
 
-app.get('/bye', (req, res) => {
-    console.log('adios')
-    res.send('Adios')
-  })
+// Crear un usuario
+app.post('/user', async (req, res) => {
+
+  const user = req.body
+  const id = await db.collection('users').add(user)
+  res.send({user, id })
+})
+
+// Obtener un usuario en concreto
+app.get('/user/:id', async (req, res) => {
+    const id = req.params.id
+    const user = await db.collection('users').doc(id).get()
+    res.send({user: user.data(), id: user.id})
+})
+
+// Updatear un usuario en concreto
+app.put('/user/:id', async (req, res) => {
+  const id = req.params.id
+  const user = req.body
+  const updateOperation = await db.collection('users').doc(id).update( user , { merge: true})
+  res.send({updateOperation})
+})
+
+// Eliminar un usuario en concreto
+app.delete('/user/:id', async (req, res) => {
+  const id = req.params.id
+  const deleteOperation = await db.collection('users').doc(id).delete()
+  res.send({deleteOperation})
+})
+
+  
 
 app.listen(port, () => { // REMOVE-ON-DEPLOY
   console.log(`Example app listening on port ${port}`) // REMOVE-ON-DEPLOY
